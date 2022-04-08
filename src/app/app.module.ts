d317334e23a9e10e551c1e238fdf5acf09b0b3c1
Natalia Tuttolomondo <n.tuttolomondo@gmail.com>
2022-04-08 00:29:34 -0300

    div to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { AsideComponent } from './componentes/aside/aside.component';
+import { SectionComponent } from './componentes/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    SeparadorComponent,
+    AsideComponent,
+    SectionComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/separador/separador.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SeparadorComponent } from './separador.component';
+
+describe('SeparadorComponent', () => {
+  let component: SeparadorComponent;
+  let fixture: ComponentFixture<SeparadorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SeparadorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SeparadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
